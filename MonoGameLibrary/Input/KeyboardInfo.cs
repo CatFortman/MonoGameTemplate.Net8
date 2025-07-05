d1fc670c01232b324e5937d40393a2524a4f88a6
cat fortman <catherine.fortman@gmail.com>
2025-07-05 17:34:35 -0300

    Add animation and input

---
+++ MonoGameLibrary/Input/KeyboardInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class KeyboardInfo
+{
+    /// <summary>
+    /// Gets the state of keyboard input during the previous update cycle.
+    /// </summary>
+    public KeyboardState PreviousState { get; private set; }
+
+    /// <summary>
+    /// Gets the state of keyboard input during the current input cycle.
+    /// </summary>
+    public KeyboardState CurrentState { get; private set; }
+
+}
