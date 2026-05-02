f31525ea00c7d8fe153da0a790a1ab50daf6474b
cat fortman <catherine.fortman@gmail.com>
2026-05-01 23:04:22 -0300

    Update BounceSystem.cs

--- MonoGameTemplate.ECS/ECS/Systems/BounceSystem.cs
+++ MonoGameTemplate.ECS/ECS/Systems/BounceSystem.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.Xna.Framework;
 using MonoGameLibrary;
 using MonoGameLibrary.ECS.Systems;
 using MonoGameLibrary.Scenes;
 using MonoGameTemplate.ECS.Components;
+using MonoGameTemplate.ECS.Interfaces;
 
 namespace MonoGameTemplate.ECS.Systems;
 
@@ -10,50 +12,58 @@ public class BounceSystem : IGameSystem
 {
     public void Update(GameContext context, GameTime gameTime, IEcsScene scene)
     {
-        var bounds = context.GraphicsDevice.Viewport.Bounds;
-        var entities = scene.Entities;
-
-        foreach (var entity in entities.Query<BounceComponent, PositionComponent, VelocityComponent, SpriteComponent>(scene.ActiveEntities))
+        if (scene is IWorldBoundsProvider provider)
         {
-            ref var position = ref entities.GetRef<PositionComponent>(entity.Id);
-            ref var velocity = ref entities.GetRef<VelocityComponent>(entity.Id);
-            var sprite = entities.Get<SpriteComponent>(entity.Id);
+            Rectangle? worldBounds = provider?.WorldBounds;
 
-            bool bounced = false;
+            if (worldBounds == null)
+                throw new InvalidOperationException("WorldBoundsSystem requires IWorldBoundsProvider");
 
-            // Horizontal bounce
-            if (position.Value.X <= 0)
-            {
-                position.Value.X = 0;
-                velocity.Value.X *= -1;
-                bounced = true;
-            }
-            else if (position.Value.X + sprite.Sprite.Width >= bounds.Width)
-            {
-                position.Value.X = bounds.Width - sprite.Sprite.Width;
-                velocity.Value.X *= -1;
-                bounced = true;
-            }
+            var bounds = worldBounds.Value;
+            var entities = scene.Entities;
 
-            // Vertical bounce
-            if (position.Value.Y <= 0)
+            foreach (var entity in entities.Query<BounceComponent, PositionComponent, VelocityComponent, SpriteComponent>(scene.ActiveEntities))
             {
-                position.Value.Y = 0;
-                velocity.Value.Y *= -1;
-                bounced = true;
-            }
-            else if (position.Value.Y + sprite.Sprite.Height >= bounds.Height)
-            {
-                position.Value.Y = bounds.Height - sprite.Sprite.Height;
-                velocity.Value.Y *= -1;
-                bounced = true;
-            }
+                ref var position = ref entities.GetRef<PositionComponent>(entity.Id);
+                ref var velocity = ref entities.GetRef<VelocityComponent>(entity.Id);
+                var sprite = entities.Get<SpriteComponent>(entity.Id);
 
-            // Play sound ONLY if bounce occurred
-            if (bounced)
-            {
-                var sound = entities.Get<BounceSoundComponent>(entity.Id);
-                sound.Sound.Play();
+                bool bounced = false;
+
+                // Horizontal bounce
+                if (position.Value.X <= bounds.Left)
+                {
+                    position.Value.X = bounds.Left;
+                    velocity.Value.X *= -1;
+                    bounced = true;
+                }
+                else if (position.Value.X + sprite.Sprite.Width >= bounds.Width)
+                {
+                    position.Value.X = bounds.Width - sprite.Sprite.Width;
+                    velocity.Value.X *= -1;
+                    bounced = true;
+                }
+
+                // Vertical bounce
+                if (position.Value.Y <= bounds.Top)
+                {
+                    position.Value.Y = bounds.Top;
+                    velocity.Value.Y *= -1;
+                    bounced = true;
+                }
+                else if (position.Value.Y + sprite.Sprite.Height >= bounds.Bottom)
+                {
+                    position.Value.Y = bounds.Bottom - sprite.Sprite.Height;
+                    velocity.Value.Y *= -1;
+                    bounced = true;
+                }
+
+                // Play sound ONLY if bounce occurred
+                if (bounced)
+                {
+                    var sound = entities.Get<BounceSoundComponent>(entity.Id);
+                    sound.Sound.Play();
+                }
             }
         }
     }
